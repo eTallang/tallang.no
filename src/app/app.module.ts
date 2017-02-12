313488b21d74a1da10d21ad04b5c30c6980c1fbf
Erik Tallang <Erik Tallang>
2017-02-12 17:50:57 +0100

    Add reference to firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { HeaderComponent } from './ui';
 import { AppComponent, PicturesComponent, MusicComponent, MoviesComponent, OtherComponent, LoginComponent } from './pages';
 import { routes } from './routes/routes';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAuKdJ5hsSN98XrByybfc5zqxnLoHaFwGY',
+  authDomain: 'tallang-267a8.firebaseapp.com',
+  databaseURL: 'https://tallang-267a8.firebaseio.com',
+  storageBucket: 'tallang-267a8.appspot.com',
+  messagingSenderId: '412607846143'
+};
 
 @NgModule({
   declarations: [
@@ -21,6 +30,7 @@ import { routes } from './routes/routes';
   ],
   imports: [
     MaterialModule.forRoot(),
+    AngularFireModule.initializeApp(firebaseConfig),
     FlexLayoutModule,
     BrowserModule,
     FormsModule,
