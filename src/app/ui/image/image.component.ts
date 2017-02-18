c264fa0fb6db1a792fc0ea2fb8a4af985084ae11
Erik Tallang <Erik Tallang>
2017-02-18 22:53:20 +0100

    Create Image UI component

---
+++ src/app/ui/image/image.component.ts
@@ -0,0 +1,44 @@
+import { Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  trigger,
+  state,
+  style,
+  transition,
+  animate } from '@angular/core';
+
+@Component({
+  selector: 'app-image',
+  templateUrl: './image.component.html',
+  styleUrls: ['./image.component.scss'],
+  animations: [
+    trigger('highlight', [
+      state('1', style({
+        filter: 'brightness(100%)',
+        transform: 'scale(1.03)'
+      })),
+      state('0',   style({
+        filter: 'brightness(70%)',
+        transform: 'scale(1.0)'
+      })),
+      transition('1 <=> 0', animate('500ms cubic-bezier(0.4, 0.0, 0.2, 1)'))
+    ])
+  ]
+})
+export class ImageComponent implements OnInit {
+  @Input() url: string;
+  @Output() clicked = new EventEmitter();
+  isHovered = 0;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  imageClicked() {
+    this.clicked.next(this.url);
+  }
+
+}
