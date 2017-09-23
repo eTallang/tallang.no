9c399e1510bd0b6f02c0a06dd094061131a42280
Erik Tallang <e.tallang@gmail.com>
2017-09-23 21:58:00 +0200

    Remove firebase from project

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,17 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpModule } from '@angular/http';
-import * as firebase from 'firebase';
 import './rxjs-dependencies';
-
-export const firebaseConfig = {
-  apiKey: 'AIzaSyAuKdJ5hsSN98XrByybfc5zqxnLoHaFwGY',
-  authDomain: 'tallang-267a8.firebaseapp.com',
-  databaseURL: 'https://tallang-267a8.firebaseio.com',
-  storageBucket: 'tallang-267a8.appspot.com',
-  messagingSenderId: '412607846143'
-};
-firebase.initializeApp(firebaseConfig);
 
 @NgModule({
   imports: [
