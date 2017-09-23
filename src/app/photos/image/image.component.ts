47ed562824a4c70be701d7e5cc1a7f7af8f742b3
Erik Tallang <e.tallang@gmail.com>
2017-09-23 12:48:26 +0200

    Simplify image animation

--- src/app/photos/image/image.component.ts
+++ src/app/photos/image/image.component.ts
@@ -7,22 +7,22 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
   styleUrls: ['./image.component.scss'],
   animations: [
     trigger('highlight', [
-      state('1', style({
+      state('on', style({
         filter: 'brightness(100%)',
         transform: 'scale(1.03)'
       })),
-      state('0',   style({
+      state('off',   style({
         filter: 'brightness(70%)',
         transform: 'scale(1.0)'
       })),
-      transition('1 <=> 0', animate('500ms cubic-bezier(0.4, 0.0, 0.2, 1)'))
+      transition('on <=> off', animate('500ms cubic-bezier(0.4, 0.0, 0.2, 1)'))
     ])
   ]
 })
 export class ImageComponent {
   @Input() url: string;
   @Output() clicked = new EventEmitter();
-  isHovered = 0;
+  mouseState = 'off';
 
   imageClicked() {
     this.clicked.next(this.url);
