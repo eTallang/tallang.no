f9c754ef8f51a06074a66f8ee97bb75c17207e8d
Erik Tallang <e.tallang@gmail.com>
2017-09-23 17:09:06 +0200

    Move movie-template to movie component

--- src/app/movies/movie/movie.component.ts
+++ src/app/movies/movie/movie.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'tallang-movie',
   templateUrl: './movie.component.html',
   styleUrls: ['./movie.component.scss']
 })
-export class MovieComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class MovieComponent {
+  @Input() movie: any;
+  @Input() rtl: boolean;
 }
