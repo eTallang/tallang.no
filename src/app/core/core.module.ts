f487ef31e7074d0d516fdc84593599fc1089df17
Erik Tallang <e.tallang@gmail.com>
2018-12-02 14:56:59 +0100

    chore: add flex layout back into the project

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import './rxjs-dependencies';
 
 @NgModule({
   imports: [
-    BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule
   ],
   exports: [
-    BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule
   ]
