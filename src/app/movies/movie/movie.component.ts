618ed5d87f944e6442064c1e3b4c6ab6d877beda
Erik Tallang <e.tallang@gmail.com>
2018-12-03 18:42:52 +0100

    feat(movies): adjust movies style

--- src/app/movies/movie/movie.component.ts
+++ src/app/movies/movie/movie.component.ts
@@ -1,11 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'tallang-movie',
   templateUrl: './movie.component.html',
   styleUrls: ['./movie.component.scss']
 })
-export class MovieComponent {
+export class MovieComponent implements OnInit {
   @Input() movie: any;
   @Input() rtl: boolean;
+
+  isMobile = false;
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit() {
+    this.breakpointObserver.observe([Breakpoints.Small, Breakpoints.XSmall]).subscribe(result => {
+      this.isMobile = result.matches;
+    });
+  }
 }
