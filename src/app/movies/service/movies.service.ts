747bebb89de4457a896978179281e0eb8fdcaefe
Erik Tallang <e.tallang@gmail.com>
2020-02-22 00:41:10 +0100

    feat(movies): get up to 50 movies

--- src/app/movies/service/movies.service.ts
+++ src/app/movies/service/movies.service.ts
@@ -12,7 +12,7 @@ export class MoviesService {
   getYoutubeVideos(): Observable<Movie[]> {
     const playlistId = 'playlistId=PL07MHRVZW5f4sH3NLOurpd5F7uOv73Lev';
     const key = 'key=AIzaSyAXBOXg5RmypyzLvZiymF2IK2KovQ9b2Sw';
-    const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&${playlistId}&${key}`;
+    const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&${playlistId}&${key}`;
     return this.http.get<Movie[]>(url).pipe(map((result: any) => result.items.map(item => item.snippet)));
   }
 }
